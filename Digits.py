5754d6a07145ac201e5c95a7c1af07fb587643dc
GwakMirae <alfo9490@naver.com>
2021-11-27 16:02:40 +0900

    fix error

--- Digits.py
+++ Digits.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
 
     a = 0
     b = 1
-    c = []
+    c = 3
 
     m = [[1]*3 for i in range(27)]
     k = [[9]*3 for i in range(3)]
@@ -77,7 +77,7 @@ if __name__ == '__main__':
         z2 = 2
         runningCorrect = 0
 
-        for i in range(1, 4):
+        for t in range(1, 4):
             validNumbers = False
             numbers = []
             while not validNumbers:
@@ -91,14 +91,15 @@ if __name__ == '__main__':
                         validNumbers = False
                         break
 
-            print("\n%-14s%-14s%-14s%-14s" %("MY GUESS", "YOUR NO.", "RESULT", "NO. RIGHT"))
+            print("\n%-14s%-14s%-14s%-14s" % ("MY GUESS", "YOUR NO.", "RESULT", "NO. RIGHT"))
 
             for number in numbers:
                 s = 0
                 myGuess = 0
-                for j in range(1,3):
-                    #What did the original author have in mind ? The first expression always results in 0 because a is always 0
-                    s1 = a*k[z2][j]+b*l[z1][j]+c*m[z][j]
+                for j in range(0, 3):
+                    # What did the original author have in mind ?
+                    # The first expression always results in 0 because a is always 0
+                    s1 = a*k[z2][j] + b*l[int(z1)][j] + c*m[int(z)][j]
                     if s < s1:
                         s = s1
                         myGuess = j
@@ -113,9 +114,9 @@ if __name__ == '__main__':
                 else:
                     runningCorrect += 1
                     result = "RIGHT"
-                    m[z][number] = m[z][number] + 1
-                    l[z1][number] = l[z1][number] + 1
-                    k[z2][number] = k[z2][number] + 1
+                    m[int(z)][number] = m[int(z)][number] + 1
+                    l[int(z1)][number] = l[int(z1)][number] + 1
+                    k[int(z2)][number] = k[int(z2)][number] + 1
                     z = z - (z / 9) * 9
                     z = 3 * z + number
                 print("\n%-14d%-14d%-14s%-14d" %(myGuess, number, result, runningCorrect))
@@ -123,12 +124,12 @@ if __name__ == '__main__':
                 z1 = z - (z / 9) * 9
                 z2 = number
         
-        #print summary report
+        # print summary report
         print()
         if runningCorrect > 10:
             print()
             print("I GUESSED MORE THAN 1/3 OF YOUR NUMBERS.")
-            print("I WIN."+ u"\u0007")
+            print("I WIN." + u"\u0007")
         elif runningCorrect < 10:
             print("I GUESSED LESS THAN 1/3 OF YOUR NUMBERS.")
             print("YOU BEAT ME.  CONGRATULATIONS *****")
